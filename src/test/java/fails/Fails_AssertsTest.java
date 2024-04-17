99aa5791c40850710f3029aa42ba2ec8b78148ac
leafar-Santos <leafarccomputacao@gmail.com>
2024-04-17 19:48:07 -0300

    Ajustes no jenkinsFile

--- src/test/java/fails/Fails_AssertsTest.java
+++ src/test/java/fails/Fails_AssertsTest.java
@@ -9,7 +9,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class Fails_AssertsTest {
 
-
+/*
     @Test
     public void deveFalhar01(){
         assertThat(1,Matchers.is(2));
@@ -34,7 +34,7 @@ public class Fails_AssertsTest {
     public void deveFalhar05(){
         assertThat(1,Matchers.is(6));
     }
-
+*/
     @Test
     public void deveFalhar06(){
         assertThat(1,Matchers.is(7));
