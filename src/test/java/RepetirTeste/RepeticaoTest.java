f31bf90bff0b9c4145f27292106f679d7f982bfd
leafar-Santos <leafarccomputacao@gmail.com>
2024-04-27 22:55:35 -0300

    Ajuste antation de repeticao

--- src/test/java/RepetirTeste/RepeticaoTest.java
+++ src/test/java/RepetirTeste/RepeticaoTest.java
@@ -9,19 +9,17 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class RepeticaoTest {
 
-        @Test
+
         @RepeatedTest(value = 3, name = "Repetindo Teste: {currentRepetition}/{totalRepetitions}")
         public void validar1() {
             Assertions.assertEquals("João", "João");
         }
 
-        @Test
         @RepeatedTest(value = 5,  name = "Repetindo Teste: {currentRepetition}/{totalRepetitions}")
         public void validar2() {
             assertThat("João", Matchers.is("João"));
         }
 
-        @Test
         @RepeatedTest(value = 5,  name = "Repetindo Teste: {currentRepetition}/{totalRepetitions}")
         public void validar3() {
             assertThat(true, Matchers.is(true));
