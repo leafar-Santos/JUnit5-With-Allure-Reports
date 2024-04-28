fd43f4846ceda6a86dadd01fddbdded4a1fb7080
leafar-Santos <leafarccomputacao@gmail.com>
2024-04-27 22:58:36 -0300

    Ajuste antation de repeticao

--- src/test/java/fails/Fails_AssertsTest.java
+++ src/test/java/fails/Fails_AssertsTest.java
@@ -10,7 +10,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class Fails_AssertsTest {
 
-/*
+
     @Test
     public void deveFalhar01(){
         assertThat(1,Matchers.is(2));
@@ -71,7 +71,7 @@ public class Fails_AssertsTest {
         assertThat(1,Matchers.is(7));
     }
 
-    */
+    
 
 
 
