573ec0a3f38c9e9b73d8bb78ec3adc0d19e0104d
leafar-Santos <leafarccomputacao@gmail.com>
2024-05-02 09:34:50 -0300

    Limpeza de codigo

--- src/test/java/allureAnotations/AnotationsTest.java
+++ src/test/java/allureAnotations/AnotationsTest.java
@@ -1,6 +1,6 @@
 package allureAnotations;
 
-import io.qameta.allure.*;
+
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -8,7 +8,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class AnotationsTest {
 
     @Test
-    @Link(name = "História para automação", url="https://www.google.com.br",type = "Jira")
+
     public void deveInformarOlinkDaTarefaNoJira(){
        assertThat(1, Matchers.is(1));
        assertThat("João", Matchers.is("João"));
@@ -16,14 +16,13 @@ public class AnotationsTest {
     }
 
     @Test
-    @Owner("Tribo-QA")
     public void deveInormarODonoDaFeature(){
         assertThat(1, Matchers.is(1));
         assertThat("João", Matchers.is("João"));
     }
 
     @Test
-    @Severity(SeverityLevel.NORMAL)
+
     public void deveInormarONiveldeSeveridadeNormal() {
         assertThat(1, Matchers.is(1));
         assertThat("João", Matchers.is("João"));
@@ -31,32 +30,27 @@ public class AnotationsTest {
 
 
     @Test
-    @Severity(SeverityLevel.BLOCKER)
     public void deveInormarONiveldeSeveridadeBLoquer() {
         assertThat(1, Matchers.is(1));
         assertThat("João", Matchers.is("João"));
     }
 
     @Test
-    @Severity(SeverityLevel.CRITICAL)
     public void deveInormarONiveldeSeveridadeCritical() {
         assertThat(1, Matchers.is(1));
         assertThat("João", Matchers.is("João"));
     }
 
     @Test
-    @Severity(SeverityLevel.MINOR)
     public void deveInormarONiveldeSeveridadeMinor() {
         assertThat(1, Matchers.is(1));
         assertThat("João", Matchers.is("João"));
     }
 
     @Test
-    @Severity(SeverityLevel.TRIVIAL)
     public void deveInormarONiveldeSeveridadeTrivial() {
         assertThat(1, Matchers.is(1));
         assertThat("João", Matchers.is("João"));
     }
-
 
 }
